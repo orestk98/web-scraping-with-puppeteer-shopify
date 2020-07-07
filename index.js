9bab35d76545bdac9cd2c1b9aa4bef164aa70521
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-07 20:36:38 +0200

    working scraper

--- index.js
+++ index.js
@@ -1,57 +1,35 @@
 const puppeteer = require('puppeteer');
 
-(async function main() {
-  try {
-    const browser = await puppeteer.launch({
-      headless: false
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-    await page.goto('https://experts.shopify.com/', {
-      waitUntil: 'networkidle2'
-    });
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const url="https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fpartners%2F"
+  await page.goto(url);
+await page.waitFor('h3.title');
+await page.waitFor('div.logo-wrapper a img');
+await page.waitFor('div.inner-container')
+  const text = await page.evaluate(() => Array.from(document.querySelectorAll('div.inner-container h3.title')).map((partner)=>partner.innerText));
+const img = await page.evaluate(() => Array.from(document.querySelectorAll('div.inner-container a img')).map(logo=>logo.src));
+    // const innerText = await page.evaluate(() => document.querySelector('p').innerText);
+const partners = await page.evaluate(()=>
+  Array.from(document.querySelectorAll('div.inner-container'))
+  .map(inner=>({
+    title: inner.querySelector('h3.title').innerText,
+    logo: inner.querySelector('a img').src
 
-    await page.click("span._1rA3U");
-    console.log('clicked');
+  })))
+
+console.log(partners);
 
 
-
-    await page.waitForSelector('h2');
-
-
-
-    const name = await page.$eval('.2dskn > h2', el => el.innerText);
-    console.log(name);
-    // const cards = await page.$$("span._1rA3U");
-    //
-    //
-    // for (const card of cards) {
-    //   card.click();
-    //   console.log('premuto');
-    //
-    //
-    //
-    //   //
-    //   const titles = await page.$$("h2._12oDh");
-    //   //   ');
-    //   for (const title of titles) {
-    //     const name = await title.$eval('h2._12oDh', h2 => h2.innerText);
-    //     console.log('name', name);
-    //   }
-    // for (const title of titles) {
-    //   console.log(title.innerHTML);
-    // }
-    // cards[0].click();
-    // await page.waitForSelector('.Polaris-Card');
-    // const lis = await page.$$('.Polaris-Card');
-    // lis[0].click();
-    //
-
-    // if (cards) {
-    //   console.log('carte selezionate');
-    // }
-
-  } catch (e) {
-    console.log('our error:', e);
-  }
+  await browser.close();
 })();
+
+
+
+
+
+
+//Array.from(document.querySelectorAll('h3.title')).map((partner)=>partner.innerText);
+
+//Array.from(document.querySelectorAll('div.logo-wrapper a img')).map(logo=>logo.src)
---
+++ test.js
@@ -0,0 +1,94 @@
+// console.log(Array.from(document.querySelectorAll('h3.title')).map((partner)=>partner.innerText));
+
+
+
+
+ //        const titoli = await page.evaluate(() => {
+ //          return document.querySelectorAll("a").inn;
+ //        })
+ //
+ //
+ // for(tito){
+ //   await page.evaluate(()=>{
+ //     console.log(titoli[i].innerText);
+ //   })
+ //
+ // }
+
+//
+//
+// let w = await page.evaluate(()=>{return titoli[i].innerText;})
+//   console.log(w);
+// }
+
+
+// console.log(titoli);
+
+
+
+
+
+
+
+
+
+
+//     await page.waitForSelector("#js-experts-mount > div > div > div > main > div._1yDJZ._3k26L > div > div:nth-child(6) > div > div > div > div:nth-child(1) > div._3pRI- > div > div:nth-child(1) > div > a > div > div.lC-5k > div.mp2Oz > div > p:nth-child(2) > span");
+//   let link = document.querySelector("#js-experts-mount > div > div > div > main > div._1yDJZ._3k26L > div > div:nth-child(6) > div > div > div > div:nth-child(1) > div._3pRI- > div > div:nth-child(1) > div > a > div > div.lC-5k > div.mp2Oz > div > p:nth-child(2) > span")
+// link.click();
+// console.log('clicked');
+
+
+    // await page.waitForSelector("span._1rA3U")
+ // let links = await page.evaluate(async ()=> {
+ //   let l = await document.querySelector();
+ // })
+ //
+
+
+
+//
+//
+//   await page.waitForSelector("h2._12oDh._4KkyP");
+//
+//     let names = await page.evaluate( ()=> {
+//
+//   let n =  document.querySelectorAll("h2._12oDh._4KkyP");
+//
+//   return n;
+// });
+//
+// for (let i = 0; i< names.length; i++){
+//     console.log(names[i]);
+//
+//   }
+//
+
+    // const cards = await page.$$("span._1rA3U");
+//  #js-experts-mount > div > div > div > main > div._1yDJZ._3k26L > div > div:nth-child(6) > div > div > div > div:nth-child(1) > div._3pRI- > div > div:nth-child(1) > div > a > div > div.lC-5k > div.mp2Oz > div > p:nth-child(2) > span
+    //
+    // for (const card of cards) {
+    //   card.click();
+    //   console.log('premuto');
+    //
+    //
+    //
+    //   //
+    //   const titles = await page.$$("h2._12oDh");
+    //   //   ');
+    //   for (const title of titles) {
+    //     const name = await title.$eval('h2._12oDh', h2 => h2.innerText);
+    //     console.log('name', name);
+    //   }
+    // for (const title of titles) {
+    //   console.log(title.innerHTML);
+    // }
+    // cards[0].click();
+    // await page.waitForSelector('.Polaris-Card');
+    // const lis = await page.$$('.Polaris-Card');
+    // lis[0].click();
+    //
+
+    // if (cards) {
+    //   console.log('carte selezionate');
+    // }
